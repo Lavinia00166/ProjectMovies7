d260c322549897ee184f54d42890a42840efe438
Gurau Lavinia <lavinia.gurau@emanuel.ro>
2021-04-21 23:32:18 +0300

    Add project files.

---
+++ ProjectMovies7/Data/ProjectMovies7Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectMovies7.Models;
+
+namespace ProjectMovies7.Data
+{
+    public class ProjectMovies7Context : DbContext
+    {
+        public ProjectMovies7Context (DbContextOptions<ProjectMovies7Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectMovies7.Models.Movie> Movie { get; set; }
+    }
+}
